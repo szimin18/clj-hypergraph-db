ed7a7e9f096a8fdf8a5eae65d62b2657f6c0b48f
me <me@example.com>
2014-04-08 17:13:42 +0200

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-hypergraph-db
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_hypergraph_db/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-hypergraph-db.core-test
+  (:require [clojure.test :refer :all]
+            [clj-hypergraph-db.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
