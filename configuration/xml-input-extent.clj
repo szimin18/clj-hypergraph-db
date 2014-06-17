9755ea3329ea605cfcd8eba2863a4f6656014e6a
Puszek_SE <puszekse@gmail.com>
2014-06-17 14:41:31 +0200

    UserDomain, AdminDomain's Contact

--- configuration/xml-input-extent.clj
+++ configuration/xml-input-extent.clj
@@ -27,117 +27,31 @@
                 (mapping [:Place-attribute] :Place)
                 (mapping [:Address-attribute] :Address)))
 
+(for [:glue:Domains :AdminDomain :Contact]
+  (add-instance :Contact
+                (mapping [:Distributed :Distributed-text-node] :Distributed)
+                (mapping [:Owner :Owner-text-node] :Owner)
+                (mapping [:WWW :WWW-text-node] :WWW)
+                (mapping [:Description :Description-text-node] :Description)
+                (mapping [:LocalID :LocalID-text-node :ID] :ID)
+                (mapping [:Name :Name-text-node] :Name)
+                (mapping [:OtherInfo :OtherInfo-text-node] :OtherInfo)
+                (mapping [:Latitude-attribute] :Latitude)
+                (mapping [:Longitude-attribute] :Longitude)
+                (mapping [:PostCode-attribute] :PostCode)
+                (mapping [:Country-attribute] :Country)
+                (mapping [:CreationTime-attribute] :CreationTime)
+                (mapping [:Validity-attribute] :Validity)))
 
+(for [:glue:Domains :UserDomain]
+  (add-instance :UserDomain (mapping [:Distributed :Distributed-text-node] :Distributed)
+                (mapping [:Owner :Owner-text-node] :Owner)
+                (mapping [:WWW :WWW-text-node] :WWW)
+                (mapping [:Description :Description-text-node] :Description)
+                (mapping [:ID :ID-text-node :ID] :ID)
+                (mapping [:Name :Name-text-node] :Name)
+                (mapping [:OtherInfo :OtherInfo-text-node] :OtherInfo)
+                (mapping [:Level-text-node] :Level)
+                (mapping [:UserManager-text-node] :UserManager)
+                (mapping [:Member-text-node] :Member)))
 
-;(class :Extension
-;       (attributes
-;         :LocalID
-;         :Key
-;         :Value))
-;
-;(class :Entity
-;       (attributes
-;         :ID
-;         :Validity
-;         :CreationTime
-;         :OtherInfo
-;         :Name))
-;
-;(class :Domain
-;       (attributes
-;         :WWW
-;         :Description)
-;       (extends :Entity))
-;
-;(class :Share
-;       (attributes
-;         :Description)
-;       (extends :Entity))
-;
-;(class :Service
-;       (attributes
-;         :StatusInfo
-;         :QualityLevel
-;         :Capability
-;         :Type
-;         :Complexity)
-;       (extends :Entity))
-;
-;(class :Location
-;       (attributes
-;         :Latitude
-;         :Longitude
-;         :PostCode
-;         :Country
-;         :Place
-;         :Address)
-;       (extends :Entity))
-;
-;(class :AdminDomain
-;       (attributes
-;         :Distributed
-;         :Owner)
-;       (extends :Domain))
-;
-;(class :UserDomain
-;       (attributes
-;         :Member
-;         :UserManager
-;         :Level)
-;       (extends :Domain))
-;
-;(class :Contact
-;       (attributes
-;         :Type
-;         :Detail)
-;       (extends :Entity))
-;
-;(class :Manager
-;       (attributes
-;         :ProductVersion
-;         :ProductName)
-;       (extends :Entity))
-;
-;(class :Resource
-;       (extends :Entity))
-;
-;(class :Endpoint
-;       (attributes
-;         :StartTime
-;         :HealthStateInfo
-;         :SupportedProfile
-;         :InterfaceVersion
-;         :InterfaceExtension
-;         :HealthState
-;         :URL
-;         :DowntimeAnnounce
-;         :QualityLevel
-;         :IssuerCA
-;         :DowntimeStart
-;         :DowntimeInfo
-;         :WSDL
-;         :ServingState
-;         :Implementor
-;         :Semantics
-;         :Technology
-;         :Capability
-;         :ImplementationName
-;         :ImplementationVersion
-;         :DowntimeEnd
-;         :TrustedCA)
-;       (extends :Entity))
-;
-;(class :Activity
-;       (extends :Entity))
-;
-;(class :Policy
-;       (attributes
-;         :Rule
-;         :Scheme)
-;       (extends :Entity))
-;
-;(class :AccessPolicy
-;       (extends :Policy))
-;
-;(class :MappingPolicy
-;       (extends :Policy))
