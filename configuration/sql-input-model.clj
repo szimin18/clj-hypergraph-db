7af4a5f5ab5cf8bda020ba36521a88ca87b295ed
szimin <szimin18@gmail.com>
2014-06-21 23:15:50 +0200

    sql "first prototype" fix

--- configuration/sql-input-model.clj
+++ configuration/sql-input-model.clj
@@ -1,9 +1,14 @@
-(table "Entity" :Entity
-       (column "ID" :ID :String :notnull))
+(table "entity" :Entity
+       (column "ID" :ID :VARCHAR_45_ :pk :notnull)
+       (column "CreationTime" :CreationTime :DATETIME)
+       (column "Validity" :Validity :BIGINT)
+       (column "Name" :Name :VARCHAR_45_))
 
-(table "EntityOtherInfo" :EntityOtherInfo
-       (column "ID" :EntityID :String :notnull)
-       (column "OtherInfo" :OtherInfo :String :notnull))
+(table "entity_other_info" :EntityOtherInfo
+       (column "EntityID" :EntityID :VARCHAR_45_ :notnull)
+       (column "OtherInfo" :OtherInfo :VARCHAR_45_ :notnull)
+       (fk "EntityOtherInfo"
+           (between :EntityID :Entity :ID)))
 
 (relation :EntityOtherInfo
           (edge :Entity :ID :*)
