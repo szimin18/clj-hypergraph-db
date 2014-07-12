faa2726accfe240b058ebbd3f0613d5a8cd826bc
szimin <szimin18@gmail.com>
2014-07-12 14:09:45 +0200

    sql config parser fix

--- src/clj_hypergraph_db/sql_parser/sql_config_parser.clj
+++ src/clj_hypergraph_db/sql_parser/sql_config_parser.clj
@@ -5,36 +5,43 @@
 (defn database
   [type & metadata]
   (def-item :database
-    :metadata metadata))
+            :metadata metadata))
 
 (defn default-credentials
   [database-name user-name password]
   (def-item :credentials
-    :database-name database-name
-    :user-name user-name
-    :password password))
+            :database-name database-name
+            :user-name user-name
+            :password password))
 
 (defn table
   [table-name table-definition & columns]
   (def-item :table
-    :table-name table-name :table-definition table-definition :columns columns))
+            :table-name table-name
+            :table-definition table-definition
+            :columns columns))
 
 (defn column
   [column-name column-definition & flags]
   (def-item :column
-    :column-name column-name :column-definition column-definition :flags flags))
+            :column-name column-name
+            :column-definition column-definition
+            :flags flags))
 
 (defn relation
   [relation-name relation-definition between-tables reffering-columns]
   (def-item :relation
-    :relation-name relation-name :relation-definition relation-definition :between between-tables :reffering reffering-columns))
+            :relation-name relation-name
+            :relation-definition relation-definition
+            :between between-tables
+            :reffering reffering-columns))
 
 (defn between
   [between-tables]
   (def-item :between-tables
-    between-tables))
+            :betbetween-tables between-tables))
 
 (defn referring
   [reffering-columns]
   (def-item :reffering-columns
-    reffering-columns))
+            :referring-columns reffering-columns))
