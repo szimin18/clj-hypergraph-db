436d1c787c43c779cd4d6763bfb63a8914c48447
Puszek_SE <puszekse@gmail.com>
2014-09-01 08:05:36 +0200

    Fixed merge problems

--- configuration/run.clj
+++ configuration/run.clj
@@ -3,9 +3,9 @@
 (input "configuration/sql-input-model.clj" :default
        (extent "configuration/sql-input-extent.clj"))
 
-;(input "configuration/xml-input-model.clj" :default
-;       (extent "configuration/xml-input-extent.clj"))
+(input "configuration/xml-input-model.clj" :default
+       (extent "configuration/xml-input-extent.clj"))
 
 
-;(output "configuration/xml-input-model.clj" ["resources/output.xml"]
-;        (extent "configuration/xml-output-extent.clj"))
+(output "configuration/xml-input-model.clj" ["resources/output.xml"]
+        (extent "configuration/xml-output-extent.clj"))
--- src/clj_hypergraph_db/xml_parser/xml_common_functions.clj
+++ src/clj_hypergraph_db/xml_parser/xml_common_functions.clj
@@ -4,54 +4,6 @@
 
 (defn string-to-file-url
   [filename]
-<<<<<<< HEAD
-  (let [path (.getAbsolutePath (File. filename))]
-    (let [path (if (= File/separatorChar \/)
-                 path
-                 (.replace path File/separatorChar \/))]
-      (let [path (if (.startsWith path "/")
-                   path
-                   (str "/" path))]
-        (str "file:" path)))))
-
-
-(defn eval-path
-  ([path input-model]
-   (eval-path path [] input-model))
-  ([path new-path model]
-   (if (zero? (count path))
-     new-path
-     (let [children-of-input-model (:children model)
-           first-of-path (first path)
-           new-child-key (first (filter
-                                  #(= first-of-path (:name (children-of-input-model %)))
-                                  (keys children-of-input-model)))]
-       (eval-path
-         (rest path)
-         (conj new-path new-child-key)
-         (children-of-input-model new-child-key))))))
-
-
-(defn eval-leaf-path
-  ([path input-model]
-   (eval-leaf-path path [] input-model))
-  ([path new-path model]
-   (if (<= (count path) 1)
-     (let [token-attributes (:attributes model)
-           attribute-name (first (filter
-                                   #(= (last path) (:name (token-attributes %)))
-                                   (keys token-attributes)))]
-       [new-path (if attribute-name attribute-name (last path)) (if attribute-name :attribute :text)])
-     (let [children-of-input-model (:children model)
-           first-of-path (first path)
-           new-child-key (first (filter
-                                  #(= first-of-path (:name (children-of-input-model %)))
-                                  (keys children-of-input-model)))]
-       (eval-leaf-path
-         (rest path)
-         (conj new-path new-child-key)
-         (children-of-input-model new-child-key))))))
-=======
   (let [path (.getAbsolutePath (File. filename))
         path (if (= File/separatorChar \/)
                path
@@ -88,4 +40,3 @@
                                                         :when (= first-of-path (:name child-token))]
                                                     [child-name child-token]))]
        (recur (rest path) (conj new-path :children new-child-key) new-child-token)))))
->>>>>>> origin/modeling-attempts
